62326fe4867a66a69ea64d1385b3ec0ad9c5c180
Tzu-Yi Liao <r08921a08@ntu.edu.tw>
2022-09-16 10:21:47 +0800

    Add 4x5 and 5x4 Format

--- table.js
+++ table.js
@@ -117,7 +117,9 @@ $(document).ready(function () {
     constructTablet();
     let table_configuration;
     //precomputed patterns
-    let top_configurations = [32570911, 16571935, 33087007, 31120927, 16572175, 33087247, 31121167, 33087127, 16572055, 33090907, 31179517, 16539325, 33054397, 32109757, 32102077, 15587005, 33092797, 33085117, 16570045, 24827581, 33061951, 32109631, 33092671, 24835135, 32109694, 33092734, 24835198, 25069495, 31585975, 32569015, 16570039, 15586999, 33085111, 32102071, 31119031, 16539199, 33054271, 16539439, 33054511, 31585855, 32568895, 31585918, 32568958, 24827518, 24827758, 24827455, 24827695, 31119151, 31119214, 31118911, 31118974, 24796735, 24796975, 28667455, 30602878, 30602815];
+    let top_configurations_4x5 = [487095, 987063, 775806, 517821, 1032957, 1003197, 1033917, 1003191, 775869, 972591, 974367, 487101, 776046, 783486, 972351, 775983, 1017783, 516861, 783423, 974607, 972414, 1033911, 517815, 775743, 972654, 972471]
+    let top_configurations_5x4 = [1019679, 510879, 933279, 1023325, 764831, 965919, 1022863, 510935, 875935, 764887, 1023263, 1022919, 965981, 1023294, 1022891, 514987, 990654, 515015, 875966, 990623, 1018783, 991119, 965950, 514959, 933310, 1018839]
+    let top_configurations_5x5 = [32570911, 16571935, 33087007, 31120927, 16572175, 33087247, 31121167, 33087127, 16572055, 33090907, 31179517, 16539325, 33054397, 32109757, 32102077, 15587005, 33092797, 33085117, 16570045, 24827581, 33061951, 32109631, 33092671, 24835135, 32109694, 33092734, 24835198, 25069495, 31585975, 32569015, 16570039, 15586999, 33085111, 32102071, 31119031, 16539199, 33054271, 16539439, 33054511, 31585855, 32568895, 31585918, 32568958, 24827518, 24827758, 24827455, 24827695, 31119151, 31119214, 31118911, 31118974, 24796735, 24796975, 28667455, 30602878, 30602815];
     let result_array = [];
 
     //tile selection
@@ -131,28 +133,28 @@ $(document).ready(function () {
     });
 
     //button 4x5
-    // $('#button_4x5').on('click', function () {
-    //     row = 4;
-    //     col = 5;
-    //     $("#input_container").empty();
-    //     constructTablet();
-    // });
+    $('#button_4x5').on('click', function () {
+        row = 4;
+        col = 5;
+        $("#input_container").empty();
+        constructTablet();
+    });
 
     //button 5x4
-    // $('#button_5x4').on('click', function () {
-    //     row = 5;
-    //     col = 4;
-    //     $("#input_container").empty();
-    //     constructTablet();
-    // });
+    $('#button_5x4').on('click', function () {
+        row = 5;
+        col = 4;
+        $("#input_container").empty();
+        constructTablet();
+    });
 
     //button 5x5
-    // $('#button_5x5').on('click', function () {
-    //     row = 5;
-    //     col = 5;
-    //     $("#input_container").empty();
-    //     constructTablet();
-    // });
+    $('#button_5x5').on('click', function () {
+        row = 5;
+        col = 5;
+        $("#input_container").empty();
+        constructTablet();
+    });
 
     //button click
     $('#button').on('click', function () {
@@ -163,6 +165,7 @@ $(document).ready(function () {
         table_configuration = 0;
         $('#input_container .active').each(function () {
             table_configuration += Math.pow(2, $(this).attr('id'));
+            console.log(table_configuration);
         });
         if(bitNumber(table_configuration) > 17){
             //to many tiles
@@ -174,6 +177,14 @@ $(document).ready(function () {
             //let html = '<div class="result_description"><span class="green">Green</span> tiles are in the right place.<br><span class="yellow">Yellow</span> tiles are missing and need to be filled by changing water tile to empty tile or by moving <span class="red">red</span> tile to it\'s place.<br><span class="red">Red</span> tiles are in wrong place and need to be moved to a <span class="yellow">yellow</span> tile</div>';
             //$("#output_container").append($(html));
             //calculate, sort and print results
+
+            if (row == 4 && col == 5)
+              top_configurations = top_configurations_4x5
+            else if (row == 5 && col == 4)
+              top_configurations = top_configurations_5x4
+            else
+              top_configurations = top_configurations_5x5
+
             top_configurations.forEach(function myFunction(configuration) {
                 let differences = tabletDifference(table_configuration, configuration);
                 let output = [configuration, differences[0], differences[1], differences[2]];
